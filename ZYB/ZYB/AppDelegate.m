4ac4c41ee517c6cbc76339b250c1c1270ce1ce05
周永波 <zhouyongbo@zhouyongbodemacbook-air.local>
2018-07-12 11:55:06 +0800

    测试commit

--- ZYB/ZYB/AppDelegate.m
+++ ZYB/ZYB/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    NSLog(@"这里是一句测试代码");
     return YES;
 }
 
